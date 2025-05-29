0a3cfc2ab7fcabcb0840b79710ab908bb7fc3d1c
DVuKindly <lenguyenvu199922@gmail.com>
2025-05-29 23:41:02 +0700

    Fix

--- BffService.API/Program.cs
+++ BffService.API/Program.cs
@@ -44,14 +44,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<AuthProxyService>();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -61,6 +53,14 @@ builder.Services.AddCors(options =>
               .AllowAnyMethod();
     });
 });
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 
 app.UseCors(); 
